8acbaa2f535da8b5352407349e937a73c9621b1f
elvislobo472 <elvislobo472@gmail.com>
2026-09-14 12:28:26 +0530

    Logic Building Practice - 1

---
+++ Java/BasicsPrac/PatternOne.java
@@ -0,0 +1,19 @@
+package Coding.Java.BasicsPrac;
+
+public class PatternOne {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+
+}
