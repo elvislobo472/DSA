c606ac4c8b8babed77b14eb88557a1086acd107c
elvislobo472 <elvislobo472@gmail.com>
2026-09-21 19:37:35 +0530

    Finding Angle between the clock hands

---
+++ Java/TrickQs/ClockAngle.java
@@ -0,0 +1,46 @@
+package Coding.Java.StringsPrac;
+
+import java.util.Scanner;
+
+public class ClockAngle {
+
+    public static void findAngle(int hour, int minute) {
+
+        if(hour < 0 || minute < 0 || hour > 23 || minute > 59){
+            throw new IllegalArgumentException("Enter valid time:");
+
+        }
+
+
+        hour = hour % 12;
+
+        double minuteAngle = minute * 6;
+        double hourAngle =  (hour * 30) + (minute * 0.5);
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+
+        angle = Math.min(angle, (360 - angle));
+
+        System.out.println(angle);
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the hours and minutes:");
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+
+        findAngle(hour,  minute);
+
+
+
+
+    }
+}
