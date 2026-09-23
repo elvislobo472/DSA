4d780843a6e6d7285b939c1b09f3961a2511adfc
elvislobo472 <elvislobo472@gmail.com>
2026-09-23 21:43:33 +0530

    SlidingWindow

--- Java/ArraysPrac/ConstantWindow.java
+++ Java/ArraysPrac/ConstantWindow.java
@@ -8,6 +8,8 @@ public class ConstantWindow {
 
         int sum = 0;
 
+            //Calculates the sum of first 4 elements
+
             for (int i = 0; i < k; i++) {
 
                 sum += arr[i];
@@ -16,7 +18,7 @@ public class ConstantWindow {
             System.out.println(sum);
 
             for (int i = k; i < arr.length; i++){
-                sum = sum - arr[i - k] + arr[i];
+                sum = sum - arr[i - k] + arr[i];   //arr[i - k] removes the element which is not part of the window ..For eg.  in the first iteration arr[4-4] = arr[0] = -1, in the second window element arr[0] is excluded as the window slided
                 System.out.println(sum);
             }
 
