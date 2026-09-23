493fa214282e15a4c33f76f151d6c9a88d856694
elvislobo472 <elvislobo472@gmail.com>
2026-09-23 23:00:27 +0530

    Generates all possible Sub-arrays from a given array

---
+++ Java/ArraysPrac/SubArraysGenerate.java
@@ -0,0 +1,31 @@
+package Coding.Java.ArraysPrac;
+
+public class SubArraysGenerate {
+
+    public static void generateSubarray(int[] arr){
+
+
+        for(int start = 0; start < arr.length; start++){
+
+            for(int end = start; end < arr.length; end++){
+
+                for(int i = start; i<= end; i++){
+                    System.out.print(arr[i] + " ");
+                }
+
+                System.out.println();
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        generateSubarray(arr);
+    }
+
+
+
+}
