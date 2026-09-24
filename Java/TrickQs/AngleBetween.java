d1d8e5739523d95ab51942f760d975db0deaabce
elvislobo472 <elvislobo472@gmail.com>
2026-09-24 23:45:47 +0530

    Angle between two lines

--- Java/TrickQs/AngleBetween.java
+++ Java/TrickQs/AngleBetween.java
@@ -5,10 +5,16 @@ public class AngleBetween {
 
     public static int findAngle(int a, int b){
 
+
+        if (a < 0 || a > 360 || b < 0 || b > 360) {
+            throw new IllegalArgumentException("Angles must be between 0 and 360");
+        }
+
+
         int difference = Math.abs(a - b);
 
 
-        return 360-difference;
+        return Math.min(difference, 360-difference);
 
 
     }
