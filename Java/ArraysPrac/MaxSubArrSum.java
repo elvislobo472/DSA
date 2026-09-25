734a2b90e7b18ab9f0f2f8d48a99fcbb8ee76220
elvislobo472 <elvislobo472@gmail.com>
2026-09-25 22:26:09 +0530

    Maximum Sum of SubArray - Kadane's Algorithm

---
+++ Java/ArraysPrac/MaxSubArrSum.java
@@ -0,0 +1,54 @@
+package Coding.Java.ArraysPrac;
+
+public class MaxSubArrSum {
+
+    public static int maxSArrSum(int[] arr) {
+
+        int maxSum = Integer.MIN_VALUE, currSum = 0;
+
+
+            currSum = 0;
+
+            for (int i = 0; i < arr.length; i++){
+
+                currSum += arr[i];
+
+                // resetting of currSum to 0 happens after determining the maxSum value because in case of array where all the elements are negatives, it's obvious that we will have a negative value as the answer
+
+                if(currSum > maxSum){
+                    maxSum = currSum;
+                }
+
+                if(currSum < 0){
+                    currSum = 0;
+                }
+
+            }
+
+
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {3, -4, 5, 4, -1, 7, -8};
+
+        int maxSum = maxSArrSum(arr);
+
+
+        System.out.println(maxSum);
+
+
+        int[] arr2 = {-4, 5, 9, -2, 2, -3, -2};
+
+        int maxSum2 = maxSArrSum(arr2);
+
+
+        System.out.println(maxSum2);
+
+
+
+    }
+}
